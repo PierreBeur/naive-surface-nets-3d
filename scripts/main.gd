1b721ac5e8798e3d9cdc4c1268e2be68de9dc404
PierreBeur <cbeurtheret@ucsd.edu>
2023-06-14 11:10:15 -0700

    Update main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -21,21 +21,41 @@ var vertices := []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Create grid points
-	var bounds := Vector3(-bb_extent + cell_offset, bb_extent + cell_offset, cell_size)
-	for x in bounds:
-		for y in bounds:
-			for z in bounds:
-				var position := Vector3(x, y, z)
+	for x in cell_resolution:
+		for y in cell_resolution:
+			for z in cell_resolution:
+				var position := get_grid_point_position_3i(x, y, z)
 				var value := get_noise_3dv(position)
-				var grid_point := Vector4(x, y, z, value)
+				var grid_point := Vector4(position.x, position.y, position.z, value)
 				grid_points.append(grid_point)
 	# Create vertices
-	bounds = Vector3(-bb_extent + cell_size, bb_extent, cell_size)
-	for x in bounds:
-		for y in bounds:
-			for z in bounds:
-				var position := Vector3(x, y, z)
-				vertices.append(position)
+	for x in cell_resolution - 1:
+		for y in cell_resolution - 1:
+			for z in cell_resolution - 1:
+				# Check that vertex is within cell with sign change
+				# Get grid points of cell
+				var cell_grid_points := [
+					get_grid_point(x,   y,   z  ),
+					get_grid_point(x+1, y,   z  ),
+					get_grid_point(x,   y+1, z  ),
+					get_grid_point(x+1, y+1, z  ),
+					get_grid_point(x,   y,   z+1),
+					get_grid_point(x+1, y,   z+1),
+					get_grid_point(x,   y+1, z+1),
+					get_grid_point(x+1, y+1, z+1)
+				]
+				# Check for sign change
+				var inside := true
+				var outside := true
+				for grid_point in cell_grid_points:
+					if grid_point.w > 0.0:
+						inside = false
+					if grid_point.w <= 0.0:
+						outside = false
+				if not inside and not outside:
+					# Create vertex
+					var position := get_vertex_position_3i(x, y ,z)
+					vertices.append(position)
 	# Draw grid points and vertices
 	multimesh.set_instance_count(len(grid_points) + len(vertices))
 	# Draw grid points
@@ -60,6 +80,26 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
 	pass
+
+
+func get_grid_point(x: int, y: int, z: int) -> Vector4:
+	return grid_points[x * cell_resolution ** 2 + y * cell_resolution + z]
+
+
+func get_grid_point_position_3i(x: int, y: int, z: int) -> Vector3:
+	return Vector3(
+		-bb_extent + cell_offset + (cell_size * x),
+		-bb_extent + cell_offset + (cell_size * y),
+		-bb_extent + cell_offset + (cell_size * z)
+	)
+
+
+func get_vertex_position_3i(x: int, y: int, z: int) -> Vector3:
+	return Vector3(
+		-bb_extent + cell_size + (cell_size * x),
+		-bb_extent + cell_size + (cell_size * y),
+		-bb_extent + cell_size + (cell_size * z)
+	)
 
 
 func get_noise_3dv(v: Vector3) -> float:
