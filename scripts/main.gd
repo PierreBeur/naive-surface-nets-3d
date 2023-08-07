d435cfb2edd52f225d6598c475af04761ab13d9b
PierreBeur <pierrebeur0@gmail.com>
2023-08-07 12:31:38 -0700

    Update main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -134,8 +134,10 @@ func build() -> void:
 					# Find which edges of cell have sign change
 					var cell_grid_edges := []
 					for edge_index in EDGE_INDICES:
-						var sign_a := signf(cell_grid_points[edge_index[0]].w)
-						var sign_b := signf(cell_grid_points[edge_index[1]].w)
+						var value_a : float = cell_grid_points[edge_index[0]].w
+						var value_b : float = cell_grid_points[edge_index[1]].w
+						var sign_a := signf(value_a)
+						var sign_b := signf(value_b)
 						cell_grid_edges.append(sign_a + sign_b == 0.0)
 					for i in len(cell_grid_edges):
 						# If edge has sign change
